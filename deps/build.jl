dfb1c37135e67183c58f1d79f683b7849f62a16f
vincentcp <vincent.coppe@gmail.com>
2018-07-24 11:37:15 +0200

    add depr code from deps

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,13 @@
 
-Pkg.clone("https://github.com/vincentcp/CardinalBSplines.git")
-Pkg.clone("https://github.com/daanhb/BasisFunctions.jl.git")
-Pkg.checkout("BasisFunctions","julia-0.7")
-Pkg.build("BasisFunctions")
-Pkg.clone("https://github.com/daanhb/FrameFun.jl.git")
-Pkg.checkout("FrameFun", "julia-0.7")
-Pkg.build("FrameFun")
-Pkg.clone("https://github.com/vincentcp/CompactTranslatesDict.jl.git")
+if VERSION < v"0.7-"
+    Pkg.clone("https://github.com/vincentcp/CardinalBSplines.git")
+    Pkg.clone("https://github.com/daanhb/FrameFun.jl.git")
+    Pkg.checkout("FrameFun", "julia-0.7")
+    Pkg.build("FrameFun")
+    Pkg.clone("https://github.com/vincentcp/CompactTranslatesDict.jl.git")
+else
+    using Pkg
+    Pkg.add("https://github.com/vincentcp/CardinalBSplines.git")
+    Pkg.add("https://github.com/daanhb/FrameFun.jl.git#julia-0.7")
+    Pkg.add("https://github.com/vincentcp/CompactTranslatesDict.jl.git")
+end
